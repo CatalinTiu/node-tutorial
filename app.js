05e06f2cb4cd403e7a8bf0b2bf4fb2d3930fcc80
Catalin <catalintium@gmail.com>
2023-05-04 15:29:48 +0300

    version with functions

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const https = require("https");
 const fs = require("fs");
 const util = require("util");
 const path = require("path");
-
-const readFile = util.promisify(fs.readFile);
+const { callback } = require("./callback_to_promise.js");
+const { convert } = require("./convertCSV.js");
 
 http
   .createServer(function (req, res) {
@@ -13,49 +13,12 @@ http
     const url = req.url;
 
     if (url === "/callback") {
-      (async () => {
-        try {
-          const content = await readFile("./json_from_api.txt", "utf-8");
-          res.write(content);
-          res.end();
-        } catch (err) {
-          console.error(err);
-        }
-      })();
-    } else if (url === "/convertcvs") {
-      const filePath = path.join(__dirname, "CSV_file.csv");
-      const f = fs.readFileSync(
-        filePath,
-        { encoding: "utf-8" },
-        function (err) {
-          console.log(err);
-        }
-      );
-
-      f = f.split("\n");
-
-      headers = f.shift().split(",");
-
-      const json = [];
-      f.forEach(function (d) {
-        tmp = {};
-        row = d.split(",");
-        for (const i = 0; i < headers.length; i++) {
-          tmp[headers[i]] = row[i];
-        }
-        json.push(tmp);
-      });
-
-      const outPath = path.join(__dirname, "newJson.json");
-      fs.writeFileSync(outPath, JSON.stringify(json), "utf8", function (err) {
-        console.log(err);
-      });
-
-      fs.readFile(__dirname + "/newJson.json", function (err, data) {
-        res.writeHead(200, { "Content-Type": " application/json" });
+      const content = callback();
+      content.then((data) => {
         res.write(data);
-        res.end();
       });
+    } else if (url === "/convertcvs") {
+      res.write(convert());
     } else if (url === "/jsonAPI") {
       const url =
         "https://gbfs.citibikenyc.com/gbfs/en/station_information.json";
@@ -70,8 +33,8 @@ http
 
           res.on("end", () => {
             fs.writeFile("json_from_api.txt", data, (err) => {
-              res.write(data);
-              res.end();
+              //   res.write(data);
+              //   res.end();
               if (err) throw err;
               console.log("success");
             });
@@ -80,6 +43,9 @@ http
         .on("error", (err) => {
           console.log("error");
         });
+    } else {
+      res.write("Choose a path!");
+      res.end();
     }
   })
   .listen(3000, function () {
--- callback_to_promise.js
+++ callback_to_promise.js
@@ -1,13 +1,17 @@
 const fs = require("fs");
 const util = require("util");
+const path = require("path");
 
 const readFile = util.promisify(fs.readFile);
+const filePath = path.join(__dirname, "json_from_api.txt");
 
-(async () => {
+const callback = async () => {
   try {
-    const content = await readFile("./json_from_api.txt", "utf-8");
-    console.log(content);
+    const content = await readFile(filePath, "utf-8");
+    return content;
   } catch (err) {
     console.error(err);
   }
-})();
+};
+
+module.exports = { callback };
